816b6d2869eae948d625160a158f5bbba93a01d1
cristianchilat <chilatcristan@gmail.com>
2025-10-16 16:07:04 +0300

    Finished ex 4 from LaboratorNR1

--- LaboratorNR1/ex4/main.cpp
+++ LaboratorNR1/ex4/main.cpp
@@ -7,13 +7,23 @@ int main() {
 
     cin >> X;
 
-    int a, x, y, z;
+    int a, x, y, z, j, i;
 
     x = X / 1000;
     y = X % 10;
     z = (X / 10) % 100;
+    j = z / 10;
+    i = z % 10;
 
     a = y * 1000 + z * 10 + x;
 
-    cout << a;
+    cout << a << endl;
+
+    a = y * 1000 + i * 100 + j * 10 + x;
+
+    cout << a << endl;
+
+    a = y * 1000 + x;
+
+    cout << a << endl;
 }
