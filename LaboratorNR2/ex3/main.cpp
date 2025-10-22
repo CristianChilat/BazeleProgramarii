388c91fd3fd9bc4ca25e22b893d4173d45aaddec
cristianchilat <chilatcristan@gmail.com>
2025-10-22 13:16:27 +0300

    Finished ex 3 and 4 from LaboratorNR2

---
+++ LaboratorNR2/ex3/main.cpp
@@ -0,0 +1,33 @@
+#import <iostream>
+
+using namespace std;
+
+int main() {
+    int X;
+
+    cout << "Introduceti un numar din 3 cifre: ";
+    cin >> X;
+
+    int digit1 = X / 100;
+    int digit2 = (X / 10) % 10;
+    int digit3 = X % 10;
+
+    int max1 = digit1;
+    if (digit2 > max1) max1 = digit2;
+    if (digit3 > max1) max1 = digit3;
+
+    int min = digit1;
+    if (digit2 < min) min = digit2;
+    if (digit3 < min) min = digit3;
+
+    int middle = digit1 + digit2 + digit3 - max1 - min;
+
+    int max2 = middle;
+    if (middle < min) max2 = min;
+
+    int largestNumber = max1 * 100 + max2 * 10 + min;
+
+    cout << "Cel mai mare numar posibil: " << largestNumber << endl;
+
+    return 0;
+}
---
+++ LaboratorNR2/ex4/main.cpp
@@ -0,0 +1,24 @@
+#import <iostream>
+
+using namespace std;
+
+int main() {
+    float a, b;
+    int c;
+
+    cout << "Ce doriți să calculați, media aritmetică sau geometrică?" << endl;
+    cin >> c;
+
+    cout << "Introduceti primul numar: ";
+    cin >> a;
+    cout << "Introduceti al doilea numar: ";
+    cin >> b;
+
+    if (c == 1) {
+        cout << "Media aritmetica este: " << (a + b) /2 << endl;
+    } else if (c == 2) {
+        cout << "Media geometrica este: " << sqrt(a * b) << endl;
+    } else {
+        cout << "Raspuns gresit!";
+    }
+}
